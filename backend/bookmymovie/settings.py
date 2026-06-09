f11723d3455a3660166273b013a0ebf9e5079147
Christeena-Geejo <christeenageejo@gmail.com>
2026-06-09 16:38:11 +0530

    chore: add STATIC_ROOT for collectstatic in production

--- backend/bookmymovie/settings.py
+++ backend/bookmymovie/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default Primary Key Field Type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
